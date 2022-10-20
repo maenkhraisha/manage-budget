ceb815b1648a4a4df05b938589ef77fd1adcd1c8
maen <maenkh@yahoo.com>
2022-10-20 18:07:37 +0300

    add url to make email confirmation work on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,5 +96,5 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://budget-app-maen.herokuapp.com/', port: 5000 }
 end
